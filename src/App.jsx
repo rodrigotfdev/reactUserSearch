1b139a654cb5ab5a1f11ade3fd361290ca0bbf5e
Rodrigo <rtf.franca@gmail.com>
2024-09-07 18:44:08 -0300

    feat: add UserSearch component

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ export default function App() {
   return (
     <div className="app bg-lightBgColor h-screen flex flex-col items-center">
       <TopMenu />
-   
+      <UserSearch />
+    
     </div>
   );
 }
@@ -27,3 +28,33 @@ function TopMenu() {
     </div>
   );
 }
+
+function UserSearch({ userQuery, setUserQuery }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = () => {
+    setUserQuery(searchTerm);
+  };
+
+  return (
+    <div className="flex items-center justify-around w-[89%] mt-6 rounded-lg bg-white h-[46px] lg:w-[730px]">
+      <RiUserSearchFill className="text-btnColor w-1/6" />
+      <input
+        type="search"
+        name=""
+        id=""
+        placeholder="Search GitHub username..."
+        className="w-4/6 h-full"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button
+        className="btn-primary rounded-lg  h-[46px] bg-btnColor font-bold text-white w-1/6"
+        onClick={handleSearch}
+      >
+        Search
+      </button>
+    </div>
+  );
+}
+
