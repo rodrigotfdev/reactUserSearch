8a2911d5967c7082c4bf67c83e660b029db7b03c
Rodrigo <rtf.franca@gmail.com>
2024-09-10 21:17:20 -0300

    feat: add Enter key press functionality for search

--- src/components/UserSearch.jsx
+++ src/components/UserSearch.jsx
@@ -9,6 +9,12 @@ export default function UserSearch({ userQuery, setUserQuery }) {
     setUserQuery(searchTerm);
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return (
     <div className="flex items-center justify-around w-[89%] md:w-[95%] mt-6 rounded-lg bg-white h-[46px] lg:w-[730px] dark:bg-darkBoxes">
       <RiUserSearchFill className="text-btnColor w-1/6" />
@@ -20,6 +26,7 @@ export default function UserSearch({ userQuery, setUserQuery }) {
         className="w-4/6 h-full dark:bg-darkBoxes"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
       <button
         className="btn-primary rounded-lg  h-[46px] bg-btnColor font-bold text-white w-1/6"
@@ -30,3 +37,6 @@ export default function UserSearch({ userQuery, setUserQuery }) {
     </div>
   );
 }
+
+// feat: add Enter key press functionality for search
+
